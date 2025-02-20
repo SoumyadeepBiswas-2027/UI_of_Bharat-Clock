9e581eeda2bbc91cdc6841b764d526a9d9a91d3e
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2025-02-20 13:14:52 +0530

    Basic_UI_of_Bharat-Clock

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ClockHeading from './Components/ClockHeading';
+import ClockSlogan from './Components/ClockSlogan';
+import CurrentTime from './Components/CurrentTime';
+import './App.css'
+function App() {
+
+  return (
+    <center>
+      <ClockHeading/>
+      <ClockSlogan/>
+      <CurrentTime/>
+    </center>)
+  };
+export default App;
